8fb35673462a74ae74443c26848ad27c542a9e36
chemazu <chukwuemekachemazu@gmail.com>
2023-12-05 11:36:21 +0100

    updates

--- client/src/components/dashboard/live-webinar/LiveWebinarMobileNav.js
+++ client/src/components/dashboard/live-webinar/LiveWebinarMobileNav.js
@@ -17,7 +17,7 @@ import {
   UPDATE_DASHBOARD_PAGE_COUNTER_TO_DEFAULT,
   TUTOR_LOGOUT,
 } from "../../../actions/types";
-import CountdownTimer from "./CountDownTimer";
+import CountdownTimer from "./dsd";
 
 import classnames from "classnames";
 
